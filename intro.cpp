a58057fc0d29414d132e76a1076b98040b77a737
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-12-24 18:02:28 +0600

    Your commit message

--- intro.cpp
+++ intro.cpp
@@ -1,7 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    printf("hello");
+int main()
+{
+    list<int> l={1,2,4,5};
+    list<int> l2(l);            // construct a list with 0 construct
+    cout << *l.begin() << endl; //
+    for (int val : l2)
+    {
+        {
+            cout << val << endl;
+        }
+    }
     return 0;
 }
