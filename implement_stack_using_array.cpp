d343da24e44f1f7f2fa9b764fd223c256b2c7bcb
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-01-11 11:55:26 +0600

    implement stack

--- implement_stack_using_array.cpp
+++ implement_stack_using_array.cpp
@@ -1,10 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-class myStack{
-    public:
-        vector<int>v; // vector is used as a stack
+class myStack
+{
+public:
+    vector<int> v; // vector is used as a stack
+    void push(int val ) // push operation void
+    { 
+        v.push_back(val); // push_back is used to add element at the end of the vector
+    }
+
+    void pop(){
+        v.pop_back(); // pop_back is used to remove element from the end of the vector 
+    }
+
+    void top(){
+        v.back(); // back() is used to get the last element of the vector
+    }
 };
 
-int main() {
+int main()
+{
     return 0;
 }
