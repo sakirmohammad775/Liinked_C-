db9b3cac0794c5fca7e439859a25265dcf93c1da
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-01-11 10:54:42 +0600

    add new list_modifiers

--- list_modifier.cpp
+++ list_modifier.cpp
@@ -1,12 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    list<int>l={10,12,15};
-    list<int>l2;
-    l2=l;
-    for(int val :l2){
-        cout<<val<<endl;
+int main()
+{
+    list<int> l = {10, 12, 15};
+    
+    // list<int>l2;
+    // l2=l;
+    // l2.assign(l.begin(),l.end());
+
+    // l.push_back(40);
+    // l.push_front(50);
+    // l.pop_back();
+    // l.pop_front();  
+
+    l.insert(next(l.begin(), 2), 100);
+    for (int val : l)
+    {
+        cout << val << endl;
     }
     return 0;
 }
