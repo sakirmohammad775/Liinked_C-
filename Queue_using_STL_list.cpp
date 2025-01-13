cffe8e782ff0dd6092ceb864a3885475f46932d9
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-01-13 21:50:38 +0600

    add queue using STL

--- Queue_using_STL_list.cpp
+++ Queue_using_STL_list.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main() {
     queue<int>q;
-    q.
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        q.push(val);
+    }
+    while(!q.empty()) {
+        cout << q.front() << endl;
+        q.pop();
+    }
     return 0;
 }
