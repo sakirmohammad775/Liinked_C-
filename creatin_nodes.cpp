8a44875a03df5dc191aa55f26b24f3db9e474dbf
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-01-17 14:50:53 +0600

    add new creating nodes

---
+++ creatin_nodes.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val) {
+        this->val = val;   // Initialize the node's value with the given 'val'
+        this->left = NULL; // Initialize next pointer to NULL
+        this->right=NULL;
+    }
+};
+int main() {
+    return 0;
+}
